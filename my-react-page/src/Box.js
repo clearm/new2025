e2830b28538671824dad89d9d0618ba1215e40e5
seed-mun <seed.mun@sparkplus.co>
2025-01-20 15:08:54 +0900

    study : 간단한 css 적용

---
+++ my-react-page/src/Box.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Box(){
+
+    return (
+        <div> 111 box 1111 </div>
+    )
+
+}
+
+
+export default Box;
---
+++ my-react-page/src/Layouts/Footer.js
@@ -0,0 +1,22 @@
+
+const Footer = () => {
+
+    return (
+        <footer>
+
+        <nav>
+            <a href='https://cocoder.tistory.com' target='_blank' rel="noreferrer"> Blog</a> |
+            <a href='https://github.com/cocoder16' target='_blank' rel="noreferrer"> Github</a>
+        </nav>
+        <p>
+            <span> 소유자 : MJH  </span><br/>
+            <span> 이메일 : abc@gmail.com</span><br/>
+            <span> Copyright 2025. MJH. All Rights Reserved.</span>
+        </p>
+
+      </footer>
+    )
+
+}
+
+export default Footer;
