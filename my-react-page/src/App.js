36d3d63e12c8a3cefb7539c318d444f4c5e8f1eb
seed-mun <seed.mun@sparkplus.co>
2025-01-20 15:14:49 +0900

    init: tailwind css 적용

--- my-react-page/src/App.js
+++ my-react-page/src/App.js
@@ -33,6 +33,13 @@ function App() {
 
   return (
     <div className="App">
+
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-500">
+          Hello, Tailwind with React!
+        </h1>
+      </div>
+
       <h2> Welcome MJH's Home    </h2>
 
       <div>
